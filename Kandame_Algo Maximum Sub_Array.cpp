f641498264cb341179f1d95986c7931414f84399
SATYAM SHARMA <112619832+satyams1228@users.noreply.github.com>
2023-01-04 12:43:05 +0530

    Create Kandame_Algo Maximum Sub_Array.cpp

---
+++ Kandame_Algo Maximum Sub_Array.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[1000];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    // Kandame_Algo Maximum Sub_Array
+    int current_sum = 0;
+    int maximum_sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        current_sum = current_sum + arr[i];
+        if (current_sum < 0)
+        {
+            current_sum = 0;
+        }
+        maximum_sum = max(current_sum, maximum_sum);
+    }
+    cout<<"Maximum Sum " << maximum_sum<<endl;
+}
